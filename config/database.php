912c4902309eef50ce9bf285ee16f74442652af1
David <davsong16@gmail.com>
2021-04-14 16:52:21 +0100

    updated database

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'davenewsapi'),
             'username' => env('DB_USERNAME', 'davenewsapi'),
@@ -51,7 +51,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
+            // 'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => array_filter([
